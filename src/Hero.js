654417dd149a00c35980f78d31c5b5a2662db513
Kimberly Stark <kimstark@protonmail.com>
2022-02-28 16:25:26 -0700

    add typewritter functionality on hero page

--- src/Hero.js
+++ src/Hero.js
@@ -1,11 +1,28 @@
 import './Hero.css';
+import TypeIt from "typeit-react";
 
 export default function Hero() {
+
     return (
         <section className="Hero flex-column container">
-            <h1 className="Hero__intro">Hi, I'm Kimberly,
+            <h1 className="Hero__intro">Hi, I'm Kimberly.
                 <br />
-                I'm a <span>UX/UI Designer.</span>
+                I'm a <TypeIt
+                    options={{
+                        cursor: false,
+                        loop: false
+                    }}
+                    getBeforeInit={(instance) => {
+                        instance.type("UX/UI Designer")
+                            .pause(750)
+                            .delete()
+                            .pause(500)
+                            .type("Web Designer")
+                            .pause(750).delete()
+                            .type("Frontend Developer.")
+                        return instance
+                    }}
+                />
             </h1>
 
             <a href="#About"><div className="arrow-down">&darr;</div></a>
@@ -14,48 +31,6 @@ export default function Hero() {
 }
 
 
-// document.addEventListener('DOMContentLoaded',function(event){
-//     // array with texts to type in typewriter
-//     var dataText = [ "Utrecht.", "Full Service.", "Webdevelopment.", "Wij zijn Codefield!"];
-
-//     // type one text in the typwriter
-//     // keeps calling itself until the text is finished
-//     function typeWriter(text, i, fnCallback) {
-//       // chekc if text isn't finished yet
-//       if (i < (text.length)) {
-//         // add next character to h1
-//        document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
-
-//         // wait for a while and call this function again for next character
-//         setTimeout(function() {
-//           typeWriter(text, i + 1, fnCallback)
-//         }, 100);
-//       }
-//       // text finished, call callback if there is a callback function
-//       else if (typeof fnCallback == 'function') {
-//         // call callback after timeout
-//         setTimeout(fnCallback, 700);
-//       }
-//     }
-//     // start a typewriter animation for a text in the dataText array
-//      function StartTextAnimation(i) {
-//        if (typeof dataText[i] == 'undefined'){
-//           setTimeout(function() {
-//             StartTextAnimation(0);
-//           }, 20000);
-//        }
-//        // check if dataText[i] exists
-//       if (i < dataText[i].length) {
-//         // text exists! start typewriter animation
-//        typeWriter(dataText[i], 0, function(){
-//          // after callback (and whole text has been animated), start next text
-//          StartTextAnimation(i + 1);
-//        });
-//       }
-//     }
-//     // start the text animation
-//     StartTextAnimation(0);
-//   });
 
 
 
