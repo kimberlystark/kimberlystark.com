0e59c2a60a52419d6c2a20614d4c95a91db003e7
Kimberly Stark <kimstark@protonmail.com>
2022-03-01 10:11:39 -0700

    add icons for footer links

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,18 @@
 import './footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faBehance, faCodepen, faLinkedin, faDribbble } from '@fortawesome/free-brands-svg-icons'
 export default function Footer() {
     return (
         <section className="Footer flex">
             <h1>Design & Code by Kimberly Stark &copy;2022 </h1>
             <div className="Footer__social flex">
-                <a href="https://github.com/kimberlystark">GitHub</a>
-                <a href="https://codepen.io/kimstark">CodePen</a>
-                <a href="https://dribbble.com/kstark">Dribbble</a>
-                <a href="https://www.linkedin.com/in/wildyogini/">LinkedIn</a>
+
+                <a href="https://github.com/kimberlystark"><FontAwesomeIcon icon={faGithub} /></a>
+                <a href="https://www.behance.net/kimberlystark"><FontAwesomeIcon icon={faBehance} /></a>
+                <a href="https://codepen.io/kimstark"><FontAwesomeIcon icon={faCodepen} /></a>
+                <a href="https://dribbble.com/kstark"><FontAwesomeIcon icon={faDribbble} /></a>
+                <a href="https://www.linkedin.com/in/wildyogini/"><FontAwesomeIcon icon={faLinkedin} /></a>
             </div>
         </section>
     )
