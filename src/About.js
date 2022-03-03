8387cdf8a3200444d554d3329b24318718f5eef9
Kimberly Stark <kimstark@protonmail.com>
2022-03-03 13:23:58 -0700

    add resume link arrow

--- src/About.js
+++ src/About.js
@@ -1,13 +1,18 @@
 import './about.css'
+import { BsArrowUpRight } from 'react-icons/bs';
 export default function About() {
     return (
         <section id="About" className="About flex-column container  ">
             <h1 className="title">ABOUT ME</h1>
             <p className="paragraph">I love the way things move and flow together.  I am passionate about creating smart experiences that are intuitive, inclusive and lead you on a delightful journey. My background in fine-arts, anthropology and education give me a unique perspective and creative outlook when developing user experiences and interfaces. </p>
-            <a><h2>Resume</h2></a>
+            <a className='flex resume-link'>
+                <h2>Resume</h2>
+                <BsArrowUpRight className='icon' />
+            </a>
         </section>
     )
 }
 
 // "I’ve always admired how thoughtful design has the power to change our world. Whether it’s through simplifying one interaction or giving users an opportunity to view things through a new lens, I love working on projects that make people happy. The thing i love most about being a UX Designer is the ability to make things stand out and differ from others to make each thing unique in their own way!"
 
+//BsArrowUpRight
